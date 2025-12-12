3d2bcef486d1115f0460d442d1d838ab761abd69
reuvendev <103184304+reuvendev@users.noreply.github.com>
2025-12-12 17:00:57 +0800

    version2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,10 @@
+// Get the hamburger button
 const hamburger = document.getElementById('hamburger');
+
+// Get the navigation menu
 const navMenu = document.getElementById('navMenu');
 
+// Toggle menu visibility when hamburger is clicked
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
